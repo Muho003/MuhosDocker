d8cde396db0435c574a1c2c2aa0f2127044b770d
Muho003 <muho14@gmail.com>
2026-02-01 13:31:22 -0500

    Modified echo server to keep connection running.

--- echo-server.py
+++ echo-server.py
@@ -10,14 +10,17 @@ def start_echo_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+        print(f"Server listening on {PORT}...")
+
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    conn.sendall(data)
 
 if __name__ == "__main__":
     start_echo_server()
